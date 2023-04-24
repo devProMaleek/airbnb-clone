a54e52c22eedbf740bb236f56d68434211e01a67
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-04-24 18:48:09 +0100

    feat: Finished Login Functionality
    1. Can get curren user
    2. Change the usermenu when login and not loggedin
    3. User can finally login with email and password credentials, and logout successfully

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,27 +1,30 @@
 'use client';
 
-import React from 'react'
-import Container from '../Container'
-import Logo from './Logo'
-import Search from './Search'
-import UserMenu from './UserMenu'
+import React from 'react';
+import Container from '../Container';
+import Logo from './Logo';
+import Search from './Search';
+import UserMenu from './UserMenu';
+import { User } from '@prisma/client';
 
-type Props = {}
+type Props = {
+  currentUser?: User | null;
+};
 
-const Navbar = (props: Props) => {
+const Navbar = ({ currentUser }: Props) => {
   return (
-    <div className='fixed w-full bg-white z-10 shadow-sm'>
-      <div className='py-4 border-b-[1px]'>
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex flex-row justify-between items-center gap-3 md:gap-0">
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
