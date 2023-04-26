c5df1375399cc6666294bb0f7abd2b941a05c0c2
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-04-26 10:03:13 +0100

    feat: Add GitHub login feature
    1. User can register with their github profile
    2. Users can login with their github profile

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -11,6 +11,7 @@ import Heading from '../Heading';
 import Input from '../Inputs/Input';
 import { toast } from 'react-hot-toast';
 import Button from '../Button';
+import { signIn } from 'next-auth/react';
 
 type Props = {};
 
@@ -76,12 +77,20 @@ const RegisterModal = (props: Props) => {
       <div className="flex flex-col gap-4 mt-3">
         <hr />
         <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}} disabled={isLoading} />
-        <Button outline label="Continue with GitHub" icon={AiFillGithub} onClick={() => {}} disabled={isLoading} />
+        <Button
+          outline
+          label="Continue with GitHub"
+          icon={AiFillGithub}
+          onClick={() => signIn('github')}
+          disabled={isLoading}
+        />
 
         <div className="text-neutral-500 text-center mt-4 font-light">
           <div className="justify-center flex flex-row items-center gap-2">
             <div>Already have an account?</div>
-            <div onClick={onClose} className="text-rose-500 cursor-pointer hover:underline">Log In</div>
+            <div onClick={onClose} className="text-rose-500 cursor-pointer hover:underline">
+              Log In
+            </div>
           </div>
         </div>
       </div>
--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -37,7 +37,7 @@ const UserMenu = ({ currentUser }: Props) => {
         >
           <AiOutlineMenu />
           <div className="hidden md:block">
-            <Avatar />
+            <Avatar src={currentUser?.image} />
           </div>
         </div>
       </div>
