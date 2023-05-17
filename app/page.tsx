68bace174e560948df9c3c74f2200cc7c49b2762
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-05-17 11:00:09 +0100

    fix: Fix types conflict

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google';
 import ClientOnly from './components/ClientOnly';
 import EmptyState from './components/EmptyState';
 import getListings from './actions/getListings';
-import { Listing } from '@prisma/client';
 import ListingCard from './components/Listings/ListingCard';
 import getCurrentUser from './actions/getCurrentUser';
 import Container from './components/Container';
@@ -26,7 +25,7 @@ export default async function Home() {
     <ClientOnly>
       <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing: Listing) => {
+          {listings.map((listing: any) => {
             return <ListingCard key={listing.id} currentUser={currentUser} data={listing} />;
           })}
       </div>
