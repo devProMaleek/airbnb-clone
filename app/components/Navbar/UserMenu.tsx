a264e531dd4e27b2b74912e7bafd5555e83391fe
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-05-24 14:18:07 +0100

    feat: Navigate to the correct route
    1. Navigate the menu option of the reservation to the right page

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -60,7 +60,7 @@ const UserMenu = ({ currentUser }: Props) => {
               <>
                 <MenuItem onClick={() => router.push('/trips')} label="My trips" />
                 <MenuItem onClick={() => {}} label="My favorites" />
-                <MenuItem onClick={() => {}} label="My reservations" />
+                <MenuItem onClick={() => router.push('/reservations')} label="My reservations" />
                 <MenuItem onClick={() => {}} label="My properties" />
                 <MenuItem onClick={rentOnOpen} label="Airbnb my home" />
                 <hr />
