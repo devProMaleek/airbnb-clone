710a65b66fb3b97d3e4a099a390598ac335eefe9
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-05-25 14:56:58 +0100

    refactor: Refactor the index page to accept searh params as props

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,22 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import ClientOnly from './components/ClientOnly';
 import EmptyState from './components/EmptyState';
-import getListings from './actions/getListings';
+import getListings, { IListingsParams } from './actions/getListings';
 import ListingCard from './components/Listings/ListingCard';
 import getCurrentUser from './actions/getCurrentUser';
 import Container from './components/Container';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default async function Home() {
-  const listings = await getListings();
+type Props = {
+  searchParams: IListingsParams;
+};
+
+const Home = async ({ searchParams }: Props) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
-    ``;
     return (
       <ClientOnly>
         <EmptyState />
@@ -24,13 +27,14 @@ export default async function Home() {
   return (
     <ClientOnly>
       <Container>
-      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {listings.map((listing) => {
             return <ListingCard key={listing.id} currentUser={currentUser} data={listing} />;
           })}
-      </div>
-
+        </div>
       </Container>
     </ClientOnly>
   );
-}
+};
+
+export default Home;
