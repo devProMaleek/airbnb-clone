83b149711652e10a01c4387bddfc6439d22f445a
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-05-27 17:41:48 +0100

    refactor: Refactor the index page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import getListings, { IListingsParams } from './actions/getListings';
 import ListingCard from './components/Listings/ListingCard';
 import getCurrentUser from './actions/getCurrentUser';
 import Container from './components/Container';
+import ScaleLoader from 'react-spinners/ScaleLoader';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,18 +21,28 @@ const Home = async ({ searchParams }: Props) => {
   if (listings.length === 0) {
     return (
       <ClientOnly>
-        <EmptyState />
+        <EmptyState showReset />
       </ClientOnly>
     );
   }
   return (
     <ClientOnly>
       <Container>
-        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing) => {
-            return <ListingCard key={listing.id} currentUser={currentUser} data={listing} />;
-          })}
-        </div>
+        {listings ? (
+          <>
+            <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+              {listings.map((listing) => {
+                return <ListingCard key={listing.id} currentUser={currentUser} data={listing} />;
+              })}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="h-[70vh] flex flex-col items-center justify-center">
+              <ScaleLoader height={50} width={7} color="red" />
+            </div>
+          </>
+        )}
       </Container>
     </ClientOnly>
   );
